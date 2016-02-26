ced19feb7014d4e6e3b7df954dfc853b672ef2ee
Jose Ramon Balsas <jrbalsas@ujaen.es>
2016-02-26 10:46:21 +0100

    Migración de proyecto Netbeans a Maven

--- src/java/daw/club/model/dao/ClienteDAO.java
+++
@@ -1,9 +0,0 @@
-package daw.club.model.dao;
-
-import daw.club.model.Cliente;
-
-public interface ClienteDAO extends GenericDAO<Cliente, Integer>{
-    //Declare here specific methods for EntityDAO
-    //public List<Cliente> findByNIF(String nif);
-    //..
-}
