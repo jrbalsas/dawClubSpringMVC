0d1c50c6672c3af62c346e01e5d93414a0c64ac4
José Ramón Balsas <jrbalsas@ujaen.es>
2019-02-28 17:35:02 +0100

    updated SpringMVC mapping annotations

--- src/main/java/com/daw/club/controller/ClienteController.java
+++ src/main/java/com/daw/club/controller/ClienteController.java
@@ -4,7 +4,6 @@ import com.daw.club.model.Cliente;
 import com.daw.club.model.dao.ClienteDAO;
 import com.daw.club.model.dao.MedioPagoDAO;
 import java.util.List;
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -13,11 +12,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -50,40 +49,42 @@ private void configView(HttpServletRequest request, HttpServletResponse response
     model.addAttribute("mediosPago", mediosPago.buscaTodos().toArray());
 }
     
-@RequestMapping(value={"","/listado"}, method=RequestMethod.GET)
+//@RequestMapping(value={"","/listado"}, method=RequestMethod.GET)
+@GetMapping(path = {"","/listado"})
 public String listado(ModelMap model) {
         List<Cliente> clientes=clienteDAO.buscaTodos();
         model.addAttribute("clientes", clientes);
         return "clientes/listado_jstl";
 }
 
-@RequestMapping(value = "/visualiza", method=RequestMethod.GET)
+
+@GetMapping("/visualiza")
 public String visualiza(@RequestParam(value="id",defaultValue="0")Integer id, ModelMap model ) {        
         model.addAttribute("cliente",clienteDAO.buscaId(id));
         return "clientes/visualiza";
 }
 
 //Sample Path info param
-@RequestMapping(value = "/visualiza/{id}", method=RequestMethod.GET)
+@GetMapping("/visualiza/{id}")
 public String visualizaId(@PathVariable Integer id, ModelMap model ) {        
         model.addAttribute("cliente",clienteDAO.buscaId(id));
         return "clientes/visualiza";
 }
 
-@RequestMapping(value = "/borra", method=RequestMethod.GET)
+@GetMapping("/borra")
 public String borra(@RequestParam(value="id",defaultValue="0")Integer id, ModelMap model) {
         clienteDAO.borra(id);
         model.clear();
         return "redirect:.";
 }
 
-@RequestMapping(value = "/crea", method=RequestMethod.GET)
+@GetMapping("/crea")
 public String creaForm(ModelMap model) {
         model.addAttribute("cliente",new Cliente());
         return "clientes/crea";
 }
 
-@RequestMapping(value = "/crea", method=RequestMethod.POST)
+@PostMapping("/crea")
 public String crea( @ModelAttribute("cliente") @Valid Cliente cliente, BindingResult result, ModelMap model) {
         String view="redirect:listado"; //default view
 
@@ -101,13 +102,13 @@ public String crea( @ModelAttribute("cliente") @Valid Cliente cliente, BindingRe
         return view;
 }
 
-@RequestMapping(value = "/edita", method=RequestMethod.GET)
+@GetMapping("/edita")
 public String editaForm(@RequestParam(value="id",defaultValue="0")Integer id, ModelMap model ) {
     model.addAttribute("cliente", clienteDAO.buscaId(id));
     return "clientes/edita";
 }
 
-@RequestMapping(value = "/edita", method=RequestMethod.POST)
+@PostMapping("/edita")
 public String edita(@ModelAttribute("cliente") @Valid Cliente cliente, BindingResult result, ModelMap model) {
     String view="redirect:visualiza/"; 
     if (!result.hasErrors()) {        
